be6643946f7e52a5739e2b22c4f3324cbf13024e
Ferfa1999 <fernando.fabbiano@gmail.com>
2020-05-14 17:49:40 -0300

    Algunos cambios.

--- main.cpp
+++ main.cpp
@@ -13,14 +13,13 @@ int main(int argc, const char* argv[]){
     std::vector<Thread*> threads;
     Spawner spawner(argv[1],cola_a,cola_m,cola_l,threads,inventario,puntos);
     spawner.read_file();
-    int cant_threads = threads.size();
     inventario.set_cant_recolectores(spawner.cantidad_recolectores);
     Map mapa(argv[2], cola_a, cola_l, cola_m);
-    for (int i = 0; i < cant_threads; i++){
+    for (int i = 0; i < (int)threads.size(); i++){
         threads[i]->start();    
     }
     mapa.repartir_recursos();
-    for (int i = 0; i < cant_threads; i++){
+    for (int i = 0; i < (int)threads.size(); i++){
         threads[i]->join();    
         delete(threads[i]);
     }
